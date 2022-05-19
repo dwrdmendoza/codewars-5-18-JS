c5819634a7d16b4d36ee89c642435226c57b055e
Edward--TacoFed-- <dwrdmendoza@yahoo.com>
2022-05-18 22:24:22 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+/* Given a set of numbers, return the additive inverse of each. Each positive becomes negative, and the negatives become positives. */
+
+function invert(array) {
+  return array.map(num => num * -1) ;
+}
